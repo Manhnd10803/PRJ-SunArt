d7fdb3fb386336e183cf5f351f4ece5fd4812358
Phan Luan <phanluandev@gmail.com>
2023-09-03 15:19:20 +0700

    toarst

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,31 +8,6 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-<<<<<<< HEAD
-
-        <!-- Bootstrap 3.3.7 -->
-        <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="{{ asset('bower_components/Ionicons/css/ionicons.min.css') }}">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
-        <!-- AdminLTE Skins. Choose a skin from the css/skins
-           folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
-        <!-- Morris chart -->
-        <link rel="stylesheet" href="{{ asset('bower_components/morris.js/morris.css') }}">
-        <!-- jvectormap -->
-        <link rel="stylesheet" href="{{ asset('bower_components/jvectormap/jquery-jvectormap.css') }}">
-        <!-- Date Picker -->
-        <link rel="stylesheet"
-            href="{{ asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
-        <!-- Daterange picker -->
-        <link rel="stylesheet" href="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
-        <!-- bootstrap wysihtml5 - text editor -->
-        <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
-=======
     <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/7d23d1769b.js" crossorigin="anonymous"></script>
@@ -55,7 +30,6 @@
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
     <!-- DataTables -->
     <link rel="stylesheet" href="{{ asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
->>>>>>> 77123c59972bfe61ce8b7bca54285749c4c77a85
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
